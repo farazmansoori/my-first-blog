3e3f5bc4df34f00a74a1d5ae83e3f55f79816c01
farazmansoori <farazman.sgei@gmail.com>
2021-12-07 13:20:46 +0530

    Dynamic Template Pushed

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from django.utils import timezone
+
+from .models import Post # Importing Post Model form medels.py. (.) Dot before models means current directory/application. This means we can use . and the name of the file (without .py).
+
 # Create your views here.
+
+'''
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+'''
+def post_list(request):
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
